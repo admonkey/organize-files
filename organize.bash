e9208681314e9c1716919a5b128d5577b2c33b71
Jeff <jeff@jeffpuckett.com>
2015-10-18 11:39:19 -0500

    Case insensitive matching

--- organize.bash
+++ organize.bash
@@ -4,6 +4,9 @@
 # get list of folders in current directory without trailing slash
 folders=(  $( ls -d */ | sed 's|[/]||g' )  )
 
+# no case sensitivity; will revert after script ends
+shopt -s nocaseglob
+
 # iterate through array of folders
 for folder in ${folders[*]}; do
 	# group same name files into folders
