2330ca0373525d10761d5947dee7a01514b033e4
Jeff <jeff@jeffpuckett.com>
2015-10-25 11:46:20 -0500

    Add logging

--- organize.bash
+++ organize.bash
@@ -9,7 +9,21 @@ shopt -s nocaseglob
 
 # iterate through array of folders
 for folder in ${folders[*]}; do
+
+	# create log variables
+	log="organize.bash.log.$folder.txt"
+	timestamp="# "$(date +"%Y-%m-%d.%A.%H:%M:%S")
+	
 	# group same name files into folders without overwriting
 	# FIX error moving folder into itself
-	mv -vn $folder* $folder/ 2>&1 | grep -v "to a subdirectory of itself"
+	mv -vn $folder* $folder/ 2>&1 | grep -v "to a subdirectory of itself" | tee temp
+
+	# if new, then clobber old log
+	# FIX: tee seems to be writing blank files
+	if [[ -f temp ]]; then
+		echo $timestamp > $log
+		cat temp >> $log
+		rm temp
+	fi
+
 done
